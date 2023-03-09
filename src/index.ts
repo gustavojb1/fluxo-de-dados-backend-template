6b292549a41c462eb1ebe791fa72002f66cd8366
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2023-03-09 16:00:51 -0300

    finished

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,131 @@
-import express, { Request, Response } from 'express'
-import cors from 'cors'
-import { accounts } from './database'
-import { ACCOUNT_TYPE } from './types'
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { accounts } from "./database";
+import { ACCOUNT_TYPE } from "./types";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.listen(3003, () => {
-    console.log("Servidor rodando na porta 3003")
-})
+  console.log("Servidor rodando na porta 3003");
+});
 
 app.get("/ping", (req: Request, res: Response) => {
-    res.send("Pong!")
-})
+  res.send("Pong!");
+});
 
 app.get("/accounts", (req: Request, res: Response) => {
-    res.send(accounts)
-})
+  res.send(accounts);
+});
 
 app.get("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+  try {
+    const id = req.params.id;
 
-    const result = accounts.find((account) => account.id === id) 
+    const result = accounts.find((account) => account.id === id);
 
-    res.status(200).send(result)
-})
+    if (!result) {
+      res.status(404); // res.statusCode = 404
+      throw new Error("Conta não encontrada verifique o 'ID'");
+    }
+
+    res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
+
+    res.send(error.message);
+  }
+});
+
+//Regra de Negócio: toda id começa com a letra 'a'
 
 app.delete("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+  try {
+    const id = req.params.id;
 
-    const accountIndex = accounts.findIndex((account) => account.id === id)
+    if (id[0] !== "a") {
+      res.status(400);
+      throw new Error("'id' inválido. Deve iniciar com a letra 'a'");
+    }
+
+    const accountIndex = accounts.findIndex((account) => account.id === id);
 
     if (accountIndex >= 0) {
-        accounts.splice(accountIndex, 1)
+      accounts.splice(accountIndex, 1);
     }
 
-    res.status(200).send("Item deletado com sucesso")
-})
+    res.status(200).send("Item deletado com sucesso");
+  } catch (error) {
+    console.log(error);
+
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
+
+    res.send(error.message);
+  }
+});
 
 app.put("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+  try {
+    const id = req.params.id;
 
-    const newId = req.body.id as string | undefined
-    const newOwnerName = req.body.ownerName as string | undefined
-    const newBalance = req.body.balance as number | undefined
-    const newType = req.body.type as ACCOUNT_TYPE | undefined
+    const newId = req.body.id as string | undefined;
+    const newOwnerName = req.body.ownerName as string | undefined;
+    const newBalance = req.body.balance as number | undefined;
+    const newType = req.body.type as ACCOUNT_TYPE | undefined;
 
-    const account = accounts.find((account) => account.id === id) 
-
-    if (account) {
-        account.id = newId || account.id
-        account.ownerName = newOwnerName || account.ownerName
-        account.type = newType || account.type
-
-        account.balance = isNaN(newBalance) ? account.balance : newBalance
+    if (newId[0] !== "a") {
+      res.status(400);
+      throw new Error("ID tem que começar com a letra 'a'");
     }
 
-    res.status(200).send("Atualização realizada com sucesso")
-})
+    if (newOwnerName.length < 3) {
+      res.status(400);
+      throw new Error("'OwnerName' deve ter pelo menos 2 caracteres");
+    }
+
+    if (newBalance !== undefined) {
+      if (typeof newBalance !== "number") {
+        res.status(400);
+        throw new Error("balance deve ser um number");
+      }
+      if (newBalance < 0) {
+        res.status(400);
+        throw new Error("balance não pode ser negativo");
+      }
+    }
+    if (newType !== undefined) {
+      if (newType !== "Ouro" && newType !== "Platina" && newType !== "Black") {
+        res.status(400);
+        throw new Error("Type deve ser uma categoria válida");
+      }
+    }
+
+    const account = accounts.find((account) => account.id === id);
+
+    if (account) {
+      account.id = newId || account.id;
+      account.ownerName = newOwnerName || account.ownerName;
+      account.type = newType || account.type;
+
+      account.balance = isNaN(newBalance) ? account.balance : newBalance;
+    }
+
+    res.status(200).send("Atualização realizada com sucesso");
+  } catch (error) {
+    console.log(error);
+
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
+
+    res.send(error.message);
+  }
+});
